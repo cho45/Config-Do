3e5310ee3c8e4b0f3e968a4b90fc2f70e3dcec50
cho45 <cho45@lowreal.net>
2011-10-24 02:24:48 +0900

    pod

--- lib/Config/Do.pm
+++ lib/Config/Do.pm
@@ -29,16 +29,22 @@ __END__
 
 =head1 NAME
 
-Config::Do - 
+Config::Do - Just do it.
 
 =head1 SYNOPSIS
 
   use Config::Do;
 
+  my $hash = config_do('config.pl');
+
 
 =head1 DESCRIPTION
 
-Config::Do is 
+Config::Do is just a utility module for 'do' file.
+
+'do EXPR' function of Perl core is not sane because it does not die when the file contains parse error or is not found.
+
+This module provides config_do function which croaks errors and ensures that the returned value is HashRef.
 
 =head1 AUTHOR
 
