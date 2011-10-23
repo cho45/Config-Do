f811f1ddb5c313e1f04a4213ab4535d46c3de686
cho45 <cho45@lowreal.net>
2011-10-24 02:09:07 +0900

    init

---
+++ lib/Config/Do.pm
@@ -0,0 +1,38 @@
+package Config::Do;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Config::Do - 
+
+=head1 SYNOPSIS
+
+  use Config::Do;
+
+
+=head1 DESCRIPTION
+
+Config::Do is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
